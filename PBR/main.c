12b92d38ed5621545b723c0763606866bd50f263
Kate Owens <kowens711@gmail.com>
2017-04-19 10:24:50 -0400

    changed to calculate feet with modf

--- PBR/main.c
+++ PBR/main.c
@@ -11,16 +11,17 @@
 
 void metersToFeetAndInches(double meters, unsigned int *ftptr, double *inptr) {
     double rawFeet = meters * 3.281;
+    double intFeet;
+    double fractionalFoot = modf(rawFeet, &intFeet);
+    double inches = fractionalFoot * 12.0;
     
-    unsigned int feet = (unsigned int)floor(rawFeet);
+    printf("%.1f meters is equal to %.1f feet and %.2f inches.\n", meters, intFeet, inches);
     
     if (ftptr) {
-    printf("Storing %u to the address %p\n", feet, ftptr);
-    *ftptr = feet;
+    printf("Storing %u to the address %p\n", intFeet, ftptr);
+    *ftptr = intFeet;
     }
     
-    double fractionalFoot = rawFeet - feet;
-    double inches = fractionalFoot * 12.0;
     
     if (inptr) {
     printf("Storing %.2f to the address %p\n", inches, inptr);
@@ -35,6 +36,14 @@ int main(int argc, const char * argv[]) {
     
     metersToFeetAndInches(meters, &feet, &inches);
     printf("%.1f meters is equal to %d feet and %.1f inches.", meters, feet, inches);
+    
+    double pi = 3.14;
+    double integerPart;
+    double fractionPart;
+    
+    fractionPart = modf(pi, &integerPart);
+    
+    printf("integerPart = %.0f, fractionPart = %.2f\n", integerPart, fractionPart);
     
     return 0;
 }
